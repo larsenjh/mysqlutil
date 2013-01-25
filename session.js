f5d0ccead1b8a1aeacea650347230e53cfd1345c
Max Nachlinger <max.nachlinger@gmail.com>
2013-01-24 20:08:35 -0800

    Insert tests clean-up

--- session.js
+++ session.js
@@ -313,7 +313,6 @@ module.exports = function (conn) {
 						});
 					});
 				}
-
 			}
 		};
 	}
--- tests/testInsert.js
+++ tests/testInsert.js
@@ -1,77 +1,60 @@
 "use strict";
+var _ = require('underscore');
 var test = require('tap').test;
 var async = require('async');
 var harness = require('./harness.js');
 
-test('a bulk INSERT using hilo should not fail', function (t) {
-	var items = [];
-	for(var i = 0; i < 40001; i++) {
-		items[i] = {
-			created: new Date(),
-			name: "This is a test"
-		};
-	}
+test('a bulk insert using hilo works', function (t) {
+	var items = generateTestItems(40001);
 	runTest(t, function (cb) {
 		harness.mysqlSession.insert('tmp', items, function (err, result) {
-			console.log(err);
-			t.notOk(err, "no errors should be thrown on insert");
+			t.notOk(err, "no errors were thrown on bulk insert");
+			t.equal(items.length, result.length, "all inserted items were returned");
 			cb();
 		});
 	});
 });
 
-test('an INSERT using hilo should not fail', function (t) {
-	var newItem = {
-		created: new Date(),
-		name: "This is a test"
-	};
+test('an insert using hilo works', function (t) {
+	var newItem = { created: new Date(), name: "This is a test" };
 	runTest(t, function (cb) {
 		harness.mysqlSession.insert('tmp', newItem, function (err, result) {
-			console.log(err);
-			t.notOk(err, "no errors should be thrown on insert");
-			t.ok(result.insertId, "result.insertId should be passed back on insert");
+			t.notOk(err, "no errors were thrown on insert");
 			cb();
 		});
 	});
 });
 
-test('an INSERT supplying a UTC date should return that date when selected', function (t) {
+test('an insert supplied with a UTC date persists that date without altering it', function (t) {
 	var now = new Date();
 	var nowUtc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(),
 		now.getUTCMinutes(), now.getUTCSeconds());
 
-	var newItem = {
-		created: nowUtc,
-		name: "This is a test"
-	};
+	var newItem = {created: nowUtc, name: "This is a test"};
 	var newId = -1;
 
 	runTest(t, function (cb) {
 		async.series([
 			function doInsert(seriesCb) {
 				harness.mysqlSession.insert('tmp', newItem, function (err, result) {
-					t.notOk(err, "no errors should be thrown on insert");
-					t.ok(result.insertId, "result.insertId should be passed back on insert");
+					t.notOk(err, "no errors were thrown on insert");
+					t.ok(result.insertId, "insertId was passed back after insert");
 					newId = result.insertId;
 					seriesCb();
 				});
 			},
 			function checkInsert(seriesCb) {
 				harness.mysqlSession.queryOne('SELECT * FROM tmp WHERE id = ?', [newId], function (selectErr, selectResult) {
-					t.equal(selectResult.created.getTime(), nowUtc.getTime(), "created should equal nowUtc when selected back out");
+					t.equal(selectResult.created.getTime(), nowUtc.getTime(), "utc date was inserted without alterations");
 					seriesCb();
 				});
-			},
-			cb
-		])
+			}
+		], cb)
 	});
 });
 
-test('INSERTs are not written inside transactions that have been rolled back', {skip:true}, function (t) {
-	var newItem = {
-		created: new Date(),
-		name: "This is a test"
-	};
+test('inserts are not written within transactions that have been rolled back', {skip: true}, function (t) {
+	var newItem = {created: new Date(), name: "This is a test"};
 	runTest(t, function (cb) {
 		async.series([
 			function (seriesCb) {
@@ -82,7 +65,7 @@ test('INSERTs are not written inside transactions that have been rolled back', {
 			},
 			function checkInsert(seriesCb) {
 				harness.mysqlSession.query('SELECT * FROM tmp', [], function (err, result1) {
-					t.equal(result1.length, 1, "1 row should be inserted.");
+					t.equal(result1.length, 1, "a row was inserted.");
 					seriesCb();
 				});
 			},
@@ -91,7 +74,7 @@ test('INSERTs are not written inside transactions that have been rolled back', {
 			},
 			function checkInsert(seriesCb) {
 				harness.mysqlSession.query('SELECT * FROM tmp', [], function (err, result2) {
-					t.equal(result2.length, 0, "no rows should be inserted.");
+					t.equal(result2.length, 0, "no rows were inserted after the transaction was rolled back.");
 					seriesCb();
 				});
 			},
@@ -100,42 +83,49 @@ test('INSERTs are not written inside transactions that have been rolled back', {
 	}, {tempTable: false});
 });
 
-test('bulk INSERT works', function (t) {
-	var items = [];
-
-	for (var i = 0; i < 10000; i++)
-		items[i] = {name: 'test ' + i, created: new Date()};
-
-	runTest(t, function (cb) {
-		harness.mysqlSession.insert('tmp', items, function (err, result) {
-			t.notOk(err, "no errors should be thrown on bulk INSERT");
-			t.equal(items.length, result.length, "The same number of rows INSERTed should be returned");
-			cb();
-		});
-	});
-});
-
-test('INSERT returns insertId', function (t) {
+test('an insert returns an insertId', function (t) {
 	runTest(t, function (cb) {
 		harness.mysqlSession.insert('tmp', {name: 'test ', created: new Date()}, function (err, result) {
-			t.ok(result.insertId, "result.insertId should be passed back on insert");
+			t.ok(result.insertId, "insertId was passed back after insert");
 			cb();
 		});
 	});
 });
+
+test('a bulk insert returns an insertId per inserted item', function (t) {
+	var items = generateTestItems(1000);
+	runTest(t, function (cb) {
+		var insertIdMissing = false;
+		harness.mysqlSession.insert('tmp', items, function (err, results) {
+			_.each(results, function (result) {
+				if (!insertIdMissing && !result.insertId)
+					insertIdMissing = true;
+			});
+			t.notOk(insertIdMissing, "an insertId was passed back for each result after insert");
+			cb();
+		});
+	});
+});
+
+function generateTestItems(amt) {
+	var items = [];
+	for (var i = 0; i < amt; i++)
+		items[i] = {name: 'test ' + i, created: new Date()};
+	return items;
+}
 
 function runTest(t, testFn, createTableOptions) {
 	createTableOptions = createTableOptions || {tempTable: true};
 	async.series([
 		harness.connect,
 		harness.dropTable,
-		function(cb) {
+		function (cb) {
 			harness.createTable(createTableOptions, cb);
 		},
 		testFn,
 		harness.dropTable,
 		harness.disconnect
-	], function(err,res) {
+	], function (err, res) {
 		t.end();
 	});
 }
