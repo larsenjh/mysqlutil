15b11b599f6b223bf50e6ee5c948c1ec58c78f7e
Max Nachlinger <max.nachlinger@gmail.com>
2013-02-19 16:39:18 -0800

    Fixed bug in utcNow function

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 exports.utcNow = function() {
 	var now = new Date();
-	return now.getUTCFullYear() + '-' + now.getUTCMonth() + 1 + '-' + now.getUTCDate() + ' ' + now.getUTCHours() + ':' +
+	return now.getUTCFullYear() + '-' + (now.getUTCMonth() + 1) + '-' + now.getUTCDate() + ' ' + now.getUTCHours() + ':' +
 		now.getUTCMinutes() + ':' + now.getUTCSeconds();
 };
