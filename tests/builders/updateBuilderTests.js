ef399567f3d9958b2ee742baf3d87eda9cf80e03
Max Nachlinger <max.nachlinger@gmail.com>
2013-03-19 21:14:00 -0700

    Update builder tests.

--- tests/builders/updateBuilderTests.js
+++ tests/builders/updateBuilderTests.js
@@ -11,6 +11,22 @@ test('Constructs a simple update using $where', function (t) {
 
 	updateBuilder({
 		item: item,
+		tableName: 'test'
+	}, function (err, res) {
+		t.notOk(err, "Does not return an error, received: " + err);
+		t.equal(res.sql, expectedSQL, "Returns the expected SQL");
+		t.deepEqual(res.values, expectedValues, "Return the expected values");
+		t.end();
+	});
+});
+
+test('Constructs a simple update using $key', function (t) {
+	var item = { name: 'Test', color: 'Blue', $key: 1 };
+	var expectedSQL = 'UPDATE test SET name = ?, color = ? WHERE id = ?;';
+	var expectedValues = _.values(item);
+
+	updateBuilder({
+		item: item,
 		tableName: 'test',
 		defaultKeyName: 'id'
 	}, function (err, res) {
@@ -21,30 +37,21 @@ test('Constructs a simple update using $where', function (t) {
 	});
 });
 
-// TODO
-test('Constructs a simple update using $key', {skip:true}, function (t) {
-	//
-});
-
 test('Constructs a simple update using $where with rules', function (t) {
 	var now = dateHelper.utcNow();
 	var item = { name: 'Test', color: 'Blue', $where: 'id = 1' };
 	var expectedValues = _.values(item).slice(0, 2).concat(now);
-	var expectedSQL = 'UPDATE test SET name = ?, color = ?, modified = ' + now + ' WHERE id = 1;';
+	var expectedSQL = 'UPDATE test SET name = ?, color = ?, modified = ? WHERE id = 1;';
 
-	var rules = [function (_item, fields, values, expressions, tableName) {
-		fields.push('modified');
-		if (expressions)
-			expressions.push(now);
-		if (values)
-			values.push(now);
+	var rules = [function (_item, tableName) {
+		if (!_item.modified)
+			_item.modified = now;
 	}];
 
 	updateBuilder({
 		item: item,
 		tableName: 'test',
-		rules: rules,
-		defaultKeyName: 'id'
+		rules: rules
 	}, function (err, res) {
 		t.notOk(err, "Does not return an error, received: "+err);
 		t.equal(res.sql, expectedSQL, "Returns the expected SQL");
@@ -53,3 +60,26 @@ test('Constructs a simple update using $where with rules', function (t) {
 	});
 });
 
+test('Constructs a simple update using $key with rules', function (t) {
+	var now = dateHelper.utcNow();
+	var item = { name: 'Test', color: 'Blue', $key: 1 };
+	var expectedSQL = 'UPDATE test SET name = ?, color = ?, modified = ? WHERE id = ?;';
+	var expectedValues = _.values(item).slice(0,2).concat(now, 1);
+
+	var rules = [function (_item, tableName) {
+		if (!_item.modified)
+			_item.modified = now;
+	}];
+
+	updateBuilder({
+		item: item,
+		tableName: 'test',
+		defaultKeyName: 'id',
+		rules: rules
+	}, function (err, res) {
+		t.notOk(err, "Does not return an error, received: " + err);
+		t.equal(res.sql, expectedSQL, "Returns the expected SQL");
+		t.deepEqual(res.values, expectedValues, "Return the expected values");
+		t.end();
+	});
+});
