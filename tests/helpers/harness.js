625c1650f1aebb41b895710b21f22e76bf038831
Max Nachlinger <max.nachlinger@gmail.com>
2013-03-20 23:20:27 -0700

    Update harness.js

--- tests/helpers/harness.js
+++ tests/helpers/harness.js
@@ -16,7 +16,7 @@ function createTmpTable(options, cb) {
 
 exports.connect = function (t) {
 	mysqlUtil.setup({
-		host: process.env.MYSQL_HOST || 'vm',//'127.0.0.1',
+		host: process.env.MYSQL_HOST || '127.0.0.1',
 		user: process.env.MYSQL_USER || 'root',
 		password: process.env.MYSQL_PASSWORD || '',
 		database: process.env.MYSQL_DATABASE || 'mysqlutil_test'
