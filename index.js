5a41976a5cecf361c255671805051fa3958b25d7
Max Nachlinger <max.nachlinger@gmail.com>
2013-03-24 21:47:10 -0700

    Exporting utils and insertModes separately.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var _ = require('lodash');
 
 module.exports = MysqlUtil;
+module.exports.utils = require('./lib/dateHelper.js');
+module.exports.insertModes = require('./lib/insertModes.js');
 
 function MysqlUtil(settings, cb) {
 	cb = cb || function(){};
 
 	this.config = _.defaults(settings, {host:'localhost', port:3306, user:'root', multipleStatements:true, waitForConnections:true});
 	this.session = require('./lib/session.js')(this.config);
-	this.insertModes = require('./lib/insertModes.js');
-	this.utils = require('./lib/dateHelper.js');
 
 	cb(null, this.session);
 }
