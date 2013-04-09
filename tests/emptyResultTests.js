5bed41d2e857e67e188754318e942efb6d9e2420
Max Nachlinger <max.nachlinger@gmail.com>
2013-04-08 21:02:50 -0700

    query() now returns [] when no rows found and on error. queryOne now returns null on now rows found or error. Added dependency on dateformat and simplified dateHelper a bit. Wrote a simple test for dateHelper. Bumped version.

--- tests/emptyResultTests.js
+++ tests/emptyResultTests.js
@@ -6,44 +6,44 @@ var harness = require('./helpers/harness.js');
 test("Connects to the database", harness.connect);
 test("Setup test table", harness.createTestTempTable);
 
-test("query() supplied SQL with logically impossible WHERE clause returns null", function (t) {
+test("query() supplied SQL with logically impossible WHERE clause returns []", function (t) {
 	harness.db.query('SELECT 1 FROM tmp WHERE 1 != 1', function (err, result) {
-		t.equal(result, null, "Result should be null.")
+		t.ok(_.isArray(result) && _.isEmpty(result), "Result be an empty array");
 		t.end();
 	});
 });
 
-test("query() with empty result returns null", function (t) {
+test("query() with empty result returns []", function (t) {
 	harness.db.query('SELECT * FROM tmp', function (err, result) {
-		t.equal(result, null, "Result should be null.")
+		t.ok(_.isArray(result) && _.isEmpty(result), "Result be an empty array");
 		t.end();
 	});
 });
 
-test("query() with invalid sql returns null result", function (t) {
+test("query() with invalid sql returns [] result", function (t) {
 	harness.db.query('SELECT 1 FROM~ tmp', function (err, result) {
-		t.equal(result, null, "Result should be null.")
+		t.ok(_.isArray(result) && _.isEmpty(result), "Result be an empty array");
 		t.end();
 	});
 });
 
 test("queryOne() supplied SQL with logically impossible WHERE clause returns null", function (t) {
 	harness.db.queryOne('SELECT 1 FROM tmp WHERE 1 != 1', function (err, result) {
-		t.equal(result, null, "Result should be null.")
+		t.equal(result, null, "Result should be null");
 		t.end();
 	});
 });
 
 test("queryOne() with empty result returns null", function (t) {
 	harness.db.queryOne('SELECT * FROM tmp', function (err, result) {
-		t.equal(result, null, "Result should be null.")
+		t.equal(result, null, "Result should be null");
 		t.end();
 	});
 });
 
 test("queryOne() with invalid sql returns null result", function (t) {
 	harness.db.queryOne('SELECT 1 FROM~ tmp', function (err, result) {
-		t.equal(result, null, "Result should be null.")
+		t.equal(result, null, "Result should be null");
 		t.end();
 	});
 });
