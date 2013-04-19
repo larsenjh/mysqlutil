bfbba6f863d864826a9a13dd60d564ac0b1dfdb9
Max Nachlinger <max.nachlinger@gmail.com>
2013-04-19 13:53:44 -0700

    Explicitly open a db connection and throw on error.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var mysql = require('mysql');
 
 module.exports = MysqlUtil;
 module.exports.utils = require('./lib/dateHelper.js');
@@ -7,14 +8,22 @@ module.exports.insertModes = require('./lib/insertModes.js');
 function MysqlUtil(settings, cb) {
 	cb = cb || function(){};
 
-	this.config = _.defaults(settings, {
+	var connection = mysql.createConnection(_.defaults(settings, {
 		host:'localhost',
 		port:3306,
 		user:'root',
 		multipleStatements:true,
 		timezone:'Z'
+	}));
+
+	connection.on('error', function(err) {
+		throw err;
 	});
 
-	this.session = require('./lib/session.js')(this.config);
-	return cb(null, this.session);
+	connection.connect(function (err) {
+		if (err) return cb(err);
+		_.extend(exports.session, require('./lib/session.js')(connection));
+		cb(err, exports.session);
+	});
 }
+exports.session = {};
