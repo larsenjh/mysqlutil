aa46516c04a7a31c46f5206293ef6a753da89bca
Max Nachlinger <max.nachlinger@gmail.com>
2013-04-20 10:46:35 -0700

    Small pool refactor.

--- index.js
+++ index.js
@@ -7,26 +7,24 @@ module.exports.insertModes = require('./lib/insertModes.js');
 function MysqlUtil(settings, cb) {
 	cb = cb || function(){};
 
-	this.config = _.defaults(settings, {
+	_.defaults(settings, {
 		host:'localhost',
 		port:3306,
 		user:'root',
 		multipleStatements:true,
 		waitForConnections:true,
 		connectionLimit: 10,
-		preFillPool: true,
 		timezone:'Z',
 		amtToPrefill: 5,
 		connectionPingIntervalSeconds: 15 * 60 // 15 minutes
 	});
 
-	this.session = require('./lib/session.js')(this.config);
+	this.session = require('./lib/session.js')(settings);
 
-	if(!this.config.preFillPool)
+	if(!settings.amtToPrefill)
 		return cb(null, this.session);
 
-	var amtToPrefill = settings.amtToPrefill || settings.connectionLimit;
-	this.session.preFillPool(amtToPrefill, function() {
+	this.session.preFillPool(function() {
 		cb(null, this.session);
 	});
 }
