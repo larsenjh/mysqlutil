e47683f5387782a937a2a6931de03e92bfde9a1d
maxnachlinger <max.nachlinger@gmail.com>
2014-12-26 14:10:01 -0800

    Added more tests and cleaned up the code a bit with Josh Larsen

--- tests/deadlockRetryTests.js
+++ tests/deadlockRetryTests.js
@@ -10,25 +10,32 @@ test("Retries the requested amount of time on deadlock errors", function (t) {
 	function f(p, cb) {
 		amtCalls++;
 
-		var err = new Error();
-		err.code = 'ER_LOCK_DEADLOCK';
-
-		return cb(err);
+		return setImmediate(function () {
+			var err = new Error();
+			err.code = 'ER_LOCK_DEADLOCK';
+			cb(err);
+		});
 	}
 
-	deadlockRetry({fn: f, args: [1], retryAmount: amtCallsExpected}, function (err, res) {
+	deadlockRetry({
+		fn: f,
+		args: [1],
+		retryAmount: amtCallsExpected
+	}, function (err, res) {
 		t.equal(amtCalls, amtCallsExpected, amtCallsExpected + ' calls should have been made, received: ' + amtCalls);
 		t.end();
 	});
 });
 
-test("Doesn't retry for non-errors", function (t) {
+test("Doesn't retry for non-deadlock errors", function (t) {
 	var amtCalls = 0;
 	var amtCallsExpected = 1;
 
 	function f(p, cb) {
 		amtCalls++;
-		return cb(new Error());
+		return setImmediate(function () {
+			cb(new Error());
+		});
 	}
 
 	deadlockRetry({fn: f, args: [1], retryAmount: amtCallsExpected}, function (err, res) {
@@ -43,11 +50,53 @@ test("Handles missing args", function (t) {
 
 	function f(cb) {
 		amtCalls++;
-		return cb(new Error());
+
+		return setImmediate(function () {
+			var err = new Error();
+			err.code = 'ER_LOCK_DEADLOCK';
+			cb(err);
+		});
+	}
+
+	deadlockRetry({fn: f, retryAmount: amtCallsExpected}, function (err, res) {
+		t.equal(amtCalls, amtCallsExpected, amtCallsExpected + ' calls should have been made, received: ' + amtCalls);
+		t.end();
+	});
+});
+
+test("Doesn't retry on success", function (t) {
+	var amtCalls = 0;
+	var amtCallsExpected = 1;
+
+	function f(cb) {
+		amtCalls++;
+
+		return setImmediate(function () {
+			cb();
+		});
 	}
 
 	deadlockRetry({fn: f}, function (err, res) {
 		t.equal(amtCalls, amtCallsExpected, amtCallsExpected + ' calls should have been made, received: ' + amtCalls);
+		t.end();
+	});
+});
+
+test("Faithfully returns result", function (t) {
+	var amtCalls = 0;
+	var amtCallsExpected = 1;
+	var expectedResult = {name: 'Test'};
+
+	function f(cb) {
+		amtCalls++;
+
+		return setImmediate(function () {
+			cb(null, expectedResult);
+		});
+	}
+
+	deadlockRetry({fn: f}, function (err, res) {
+		t.deepEqual(res, expectedResult, "Should have returned: " + JSON.stringify(expectedResult) + " received: " + JSON.stringify(res));
 		t.end();
 	});
 });
